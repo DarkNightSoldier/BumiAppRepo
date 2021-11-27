948e200af166d17d4b2729627c4709a04864993b
Alejandro Higuera C <ahiguerac@unal.edu.co>
2021-11-26 19:11:42 -0500

    Nuevo registro de tiempos y clase pedido

--- src/registro_tiempos.py
+++ src/registro_tiempos.py
@@ -1,24 +1,38 @@
 import pandas
 import time
+from IPython import display
+import matplotlib.pyplot as plt
 
 class RegistrodeTiempos:
 
-    def __init__(self,nombre_estructura,nombre_funcionalidad,n_datos):
-        self.registro = list()
-        self.nombre_estructura = nombre_estructura
+    def __init__(self,nombre_funcionalidad):
+        self.nombre_estructura = ""
         self.nombre_funcionalidad = nombre_funcionalidad
-        self.n_datos = n_datos
+        self.n_datos = 0
 
         self.tiempo_inicio = 0
         self.tiempo_fin = 0
-    
-    def iniciar_conteo(self):
+
+        try:
+            self.datos = pandas.read_csv("asintotico/{}.csv".format(self.nombre_funcionalidad),index_col="Numero de datos")
+        except:
+            self.datos = pandas.DataFrame(columns=["Array","LLT","DLL","CDLL","ColaArray","ColaLLT","ColaDLL","ColaCDLL"],
+            index=[10**4,10**5,10**6,10**7])
+            self.datos.index.name = "Numero de datos"
+
+    def iniciar_conteo(self,n,nombre_estructura):
         self.tiempo_inicio = time.time()
+        self.nombre_estructura = nombre_estructura
+        self.n_datos = n
 
     def finalizar_conteo(self):
         self.tiempo_fin = time.time()
-        self.registro.append(self.tiempo_inicio-self.tiempo_fin)
+        self.datos[self.nombre_estructura][self.n_datos] = self.tiempo_fin-self.tiempo_inicio
+        self.datos.to_csv("asintotico/{}.csv".format(self.nombre_funcionalidad))
 
-    def retornar_registro(self):
-        datos = pandas.DataFrame(data=self.registro,index=range(1,len(self.registro)+1))
-        datos.to_csv("{}_{}_{}_datos.csv".format(self.nombre_funcionalidad,self.nombre_estructura,self.n_datos),index=range(0,len(self.registro)+1))
+    def retornar_grafica(self,estructuras_de_datos):
+        self.datos[estructuras_de_datos].plot()
+        plt.title("{}".format(self.nombre_funcionalidad))
+        plt.ylabel("Tiempo (segundos)")
+        plt.savefig("asintotico/{}.png".format(self.nombre_funcionalidad))
+
